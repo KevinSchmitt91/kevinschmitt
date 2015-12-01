317190bc63a8b6dccc21afebd8a043c22706bd08
Kevin Schmitt <kevin.schmitt@mni.thm.de>
2015-12-01 15:53:03 +0100

    added pages

--- contact.php
+++ contact.php
@@ -1,4 +1,12 @@
 <?php
+	
+	$min = 1;
+	$max = 20;
+	$number1 = rand ($min,$max);
+	$number2 = rand ($min,$max);
+	$antispam = "Was ist $number1 + $number2 ? (Anti-Spam)";
+	$erg = $number1 + $number2;
+	
 	if (isset($_POST["submit"])) {
 		$name = $_POST['name'];
 		$email = $_POST['email'];
@@ -25,7 +33,7 @@
 			$errMessage = 'Bitte geben Sie eine Nachricht ein';
 		}
 		//Check if simple anti-bot test is correct
-		if ($human !== 4) {
+		if ($human !== ($number1 + $number2)) {
 			$errHuman = 'Ihr Anti-Spam ist nicht korrekt';
 		}
 // If there are no errors, send the email
@@ -88,19 +96,16 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right cl-effect-13">
                     <li>
-                        <a class="" href="index.html">Home</a>
+                        <a class="" href="index.html"><span class="fa fa-home"></span> Home</a>
                     </li>
                     <li>
-                        <a class="" href="about.html">Über</a>
+                        <a href="index.html#services"><span class="fa fa-gear"></span> Leistungen</a>
                     </li>
                     <li>
-                        <a href="index.html#services">Leistungen</a>
+                        <a href="portfolio.html"><span class="fa fa-book"></span> Portfolio</a>
                     </li>
                     <li>
-                        <a href="portfolio.html">Portfolio</a>
-                    </li>
-                    <li>
-                        <a class="active" href="contact.php">Kontakt</a>
+                        <a class="active" href="contact.php"><span class="fa fa-comment"></span> Kontakt</a>
                     </li>
                 </ul>
             </div>
@@ -124,11 +129,12 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
                     <div class="row">
                       	<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 	                      	<div class="text-left contact-intro">
-						  		<p>Was auch immer Sie benötigen oder Ihre Ziele auch sein mögen, treten Sie mit mir in Verbindung und ich versuche Ihnen zu helfen, sich voranzubringen. Egal ob Sie ihr Porjekt mit mir besprechen wollen oder einfach nur um Hallo zu sagen, zögern Sie nicht! Schreiben Sie mir eine Nachricht auf <a href="http://www.twitter.com/kevinschmitt91" target="_blank">Twitter</a>, schreiben Sie mir eine Email an <a href="mailto:ich@kevinschmitt.info">ich@kevinschmitt.info</a> oder nutzen Sie das unten stehende Kontaktformular.</p>
+						  		<p>Was auch immer Sie benötigen oder Ihre Ziele auch sein mögen, treten Sie mit mir in Verbindung und ich versuche Ihnen zu helfen, ihr Projekt voranzubringen. Egal ob Sie ihr Projekt mit mir besprechen wollen oder einfach nur um Hallo zu sagen, zögern Sie nicht! Schreiben Sie mir eine Nachricht auf <a href="http://www.twitter.com/kevinschmitt91" target="_blank">Twitter</a>, schreiben Sie mir eine Email an <a href="mailto:ich@kevinschmitt.info">ich@kevinschmitt.info</a> oder nutzen Sie das unten stehende Kontaktformular.</p>
                   			</div>
                   			<!-- Kontakt Form -->
                   			<h3 class="uppercase">Nachricht schreiben</h3>
                   			<div class="divider-large"></div>
+                  			<p><small>Die mit <i style="color:#1773c7; font-size: 1.5em;">*</i> gekennzeichneten Felder müssen ausgefüllt sein!</small></p>
                   			<form method="post" action="" class="form">
 								<div class="text-center">
 			                        <div class="form-group">
@@ -141,7 +147,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 			                      <?php echo "<p class='text-danger'>$errName</p>";?>
 			                    </div>
 			                    <div class="input-group-lg">
-			                      <label class="pull-left" for="email">Email Addresse<i>*</i></label>
+			                      <label class="pull-left" for="email">Email Adresse<i>*</i></label>
 			                      <input name="email" type="email" class="form-control" placeholder="ich@beispiel.de">
 			                      <?php echo "<p class='text-danger'>$errEmail</p>";?>
 			                      </div>
@@ -155,8 +161,8 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 			                      <?php echo "<p class='text-danger'>$errMessage</p>";?>
 			                    </div>
 			                    <div class="input-group-lg">
-			                      <label class="pull-left" for="human">Was ist 2 + 2? (Anti-spam)<i>*</i></label>
-			                      <input name="human" type="text" class="form-control" placeholder="2 + 2 = ?">
+			                      <label class="pull-left" for="human"><?php echo $antispam;?> <i>*</i></label>
+			                      <input name="human" type="text" class="form-control" placeholder="Ergebnis">
 			                      <?php echo "<p class='text-danger'>$errHuman</p>";?>
 			                    </div>
 			                    <div class="text-center">
@@ -176,7 +182,7 @@ if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
           <ul class="list-inline social">
             <li><a href="https://facebook.com/kevin.schmitt.16" target="_blank" class="social-facebook"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></i></a></li>
           <li><a href="https://twitter.com/kevinschmitt91" target="_blank" class="social-twitter"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></i></a></li>
-          <li><a href="mailto:Schmitt.Kevin@Outlook.com" class="social-mail"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></i></a></li>
+          <li><a href="mailto:ich@kevinschmitt.info" class="social-mail"><i class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></i></a></li>
               <li><a href="impressum.html" class="social-impressum">Impressum</a></li>
         </ul>
             <p>Made with <span class="fa fa-heart animated pulse"></span> in Fernwald, Germany.</p>
